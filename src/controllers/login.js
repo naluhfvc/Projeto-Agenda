fa6e1688bc8f1504a7ded8bfeccfa8093a543a89
naluhfvc <analufavacho@gmail.com>
2022-12-28 18:19:49 -0300

    inserindo flash messages no register

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,7 +1,22 @@
+const Login = require('../models/Login')
+
 exports.index = (req, res) => {
     res.render('login')
 }
 
-exports.register = (req, res) => {
-    res.send('Olá')
+exports.register = async (req, res) => {
+    const login = new Login(req.body)
+    await login.register()
+
+    if(login.errors.length > 0){
+        req.flash('errors', login.errors)
+
+        req.session.save(()=>{
+            return res.redirect('/login/');
+        })
+
+        return 
+    }
+
+    res.send(login.errors)
 }
