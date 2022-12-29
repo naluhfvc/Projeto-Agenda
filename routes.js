47e1ff873e5938ee41416273632bbc4b054ac233
naluhfvc <analufavacho@gmail.com>
2022-12-28 23:58:15 -0300

    iniciando cadastro de contatos

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/home');
 const loginController = require('./src/controllers/login');
+const contatoController = require('./src/controllers/contato');
+
+const { loginRequired } = require('./src/middlewares/middleware')
 
 // Rotas da home
 route.get('/', homeController.index);
@@ -12,6 +15,9 @@ route.get('/login/', loginController.index)
 route.post('/login/register/', loginController.register)
 route.post('/login/signin/', loginController.signin)
 route.get('/logout/', loginController.logout)
+
+//Rotas de contato
+route.get('/contato', loginRequired, contatoController.index);
 
 
 module.exports = route;
